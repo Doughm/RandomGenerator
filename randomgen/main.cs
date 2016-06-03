33972b3f359c7f953a81a2d16400ee518e551d78
Douglas Harvey-Marose <dougharveymarose@gmail.com>
2016-06-03 14:51:08 -0700

    Version 1.0
    Random number generator in C#

---
+++ randomgen/main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace randomgen
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            //RandomPRNG rand = new RandomPRNG();
+            RandomGen randMT = new RandomGen();
+
+            //Console.WriteLine(randMT.arrayToString());
+            //Console.WriteLine(randMT.arrayToString(0));
+            //Console.WriteLine(randMT.arrayToString(1));
+            //Console.WriteLine(randMT.getNext());
+            //Console.WriteLine(randMT.getNextDecimal());
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(randMT.getNext());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
